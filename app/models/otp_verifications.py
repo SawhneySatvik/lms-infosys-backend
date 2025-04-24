eb4fc446b36f4bfd04298e5162965f6df22ebc86
SawhneySatvik <satvik.sawhney2005@gmail.com>
2025-04-24 22:25:57 +0530

    OTP Table

---
+++ app/models/otp_verifications.py
@@ -0,0 +1,16 @@
+from .. import db
+from datetime import datetime
+import uuid
+
+class OTPVerification(db.Model):
+    __tablename__ = 'otp_verifications'
+
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('auth.users.id', ondelete='CASCADE'), nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    otp = db.Column(db.Text, nullable=False)
+    expires_at = db.Column(db.DateTime(timezone=True), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<OTPVerification user_id={self.user_id}, email={self.email}, otp={self.otp}, expires_at={self.expires_at}>"
