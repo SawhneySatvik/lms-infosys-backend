25c8c2857fbca32ba4517a7281dd4b05c59ebf05
SawhneySatvik <satvik.sawhney2005@gmail.com>
2025-04-27 11:38:03 +0530

    OTP fix

--- app/models/otp_verifications.py
+++ app/models/otp_verifications.py
@@ -6,7 +6,7 @@ class OTPVerification(db.Model):
     __tablename__ = 'otp_verifications'
 
     id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('auth.users.id', ondelete='CASCADE'), nullable=False)
+    user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
     email = db.Column(db.Text, nullable=False)
     otp = db.Column(db.Text, nullable=False)
     expires_at = db.Column(db.DateTime(timezone=True), nullable=False)
