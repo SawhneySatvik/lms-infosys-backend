b5d51acdc79a30b699862a0c31de1dae8c396f48
SawhneySatvik <satvik.sawhney2005@gmail.com>
2025-04-27 19:37:39 +0530

    Updated model with access tokan and refresh token

--- app/models/otp_verifications.py
+++ app/models/otp_verifications.py
@@ -9,8 +9,10 @@ class OTPVerification(db.Model):
     user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('users.user_id', ondelete='CASCADE'), nullable=False)
     email = db.Column(db.Text, nullable=False)
     otp = db.Column(db.Text, nullable=False)
+    access_token = db.Column(db.Text, nullable=True)  # Added for login tokens
+    refresh_token = db.Column(db.Text, nullable=True)  # Added for login tokens
     expires_at = db.Column(db.DateTime(timezone=True), nullable=False)
-    created_at = db.Column(db.DateTime(timezone=True), default=datetime.utcnow)
+    created_at = db.Column(db.DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
 
     def __repr__(self):
         return f"<OTPVerification user_id={self.user_id}, email={self.email}, otp={self.otp}, expires_at={self.expires_at}>"
